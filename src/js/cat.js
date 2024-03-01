8d970f2c0350603b60459ffe7dfdb68065bcb239
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-03-01 17:42:21 +0100

    favorites working

--- src/js/cat.js
+++ src/js/cat.js
@@ -1,80 +1,56 @@
-/*let imgBreedUrl =  "https://api.thecatapi.com/v1/images/search?breed_ids=";
 
+let breed = decodeURIComponent(window.location.href.split('=')[1]);
+breed = breed.split('&')[0]; 
 
-function getCatInfo(catBreed) {
-    return new Promise(function(resolve, reject) {
-        getCats(catUrl)
-            .done(function(data) {
-                let catsData = data.data;
-                let catInfo = catsData.find(cat => cat.breed.toLowerCase() === catBreed.toLowerCase());
-                resolve(catInfo);
-            })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                reject(errorThrown);
-            });
-    });
+let id = decodeURIComponent(window.location.href.split('=')[2]);
+
+console.log(breed);
+console.log(id);
+
+let catUrl = 'https://catfact.ninja/breeds';
+
+let catPage = $('#cat-info');
+
+function getCats(url) {
+    return $.getJSON(url);
 }
+
 
 function createCatPage(catInfo) {
-    let catPage = $('#cat-info');
-    let catInfoHtml = `
-    <div  class="flex flex-col justify-center ">
+    let catPage = $('<div class="flex flex-col justify-center"></div>');
 
-        <h1 class="text-3xl font-bold text-center">${catInfo.breed}</h1>
-        <div id="cat-description" class="flex flex-col justify-center p-6 m-6 border rounded-2xl">
-        <div>
-            <h2 class="text-2xl font-semibold text-gray-500">Origin</h2>
-            <p>${catInfo.origin}</p>
-        </div>
-        <div>
-            <h2 class="text-2xl font-semibold text-gray-500">Coat</h2>
-            <p>${catInfo.coat}</p>
-        </div>
-        <div>
-            <h2 class="text-2xl font-semibold text-gray-500">Pattern</h2>
-            <p>${catInfo.pattern}</p>
-        </div>
-        </div>
-    
-        </div>
-    `;
-    catPage.append(catInfoHtml);
+    let catInfoHtml = $('<h1 class="text-3xl font-bold text-center"></h1>').text(breed);
+    let catDescription = $('<div id="cat-description" class="flex flex-col justify-center p-6 m-6 border rounded-2xl"></div>');
+
+    let origin = $('<div><h2 class="text-2xl font-semibold text-gray-500">Origin</h2><p></p></div>').find('p').text(catInfo.origin).end();
+    let coat = $('<div><h2 class="text-2xl font-semibold text-gray-500">Coat</h2><p></p></div>').find('p').text(catInfo.coat).end();
+    let pattern = $('<div><h2 class="text-2xl font-semibold text-gray-500">Pattern</h2><p></p></div>').find('p').text(catInfo.pattern).end();
+
+    catDescription.append(origin, coat, pattern);
+    catPage.append(catInfoHtml, catDescription);
+
+    $('#cat-info').append(catPage);
 }
 
-let breed = decodeURIComponent(window.location.href.split('=')[1]); // Decodificar URI
 
 getCatInfo(breed)
-    .then((catInfo) => {
+    .then(function(catInfo) {
         createCatPage(catInfo);
     })
-    .catch((error) => {
-        console.error('Error en la petición:', error);
+    .catch(function(error) {
+        console.error('Error:', error);
     });
 
-
-
-
-function getBreedImg(breed) {
-    breed = breed.toLowerCase();
-    breed = breed.substring(0, 4); //es porque en la api no hay imagenes para todas las razas
-    return $.getJSON(imgBreedUrl + breed);
-}
-
-let imgNumber = 3;
-
-for(let i = 0; i < imgNumber; i++){
-    getBreedImg(breed)
+getBreedImg(id)
     .done((data)=>{
         let img = data[0].url;
         let imgElement = $('<img>').attr('src', img);
-        //añade calses para que la imagen sea pequeña 
-        imgElement.addClass('w-1/4 h-1/4');
+        imgElement.addClass('h-96');
 
         $('#cat-img').append(imgElement);
+        console.log(id);
     })
     .fail(function(jqXHR, textStatus, errorThrown){
         console.error('Error en la petición:', textStatus, errorThrown);
     });
-}
 
-*/
--- src/js/global.js
+++ src/js/global.js
@@ -1,20 +1,80 @@
-let catUrl = 'https://catfact.ninja/breeds';
-let catBreedsUrl =  "https://api.thecatapi.com/v1/breeds?limit=25";
+let imgBreedUrl =  "https://api.thecatapi.com/v1/images/search?breed_ids=";
+
 
 function getCats(url) {
     return $.getJSON(url); 
 }
 
-function getCatsBreeds(url) {
-    //hacer todo aqui 
+function getBreedImg(breed){
+    console.log(imgBreedUrl + breed);
+    return $.getJSON(imgBreedUrl + breed);
 }
 
-/*
-getBreeds(breedsUrl)
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.error('Error en la petición:', error);
+function getCatInfo(catBreed) {
+    return new Promise(function(resolve, reject) {
+        getCats(catUrl)
+            .done(function(data) {
+                let catsData = data.data;
+                let catInfo = catsData.find(cat => cat.breed === catBreed);
+
+                if (catInfo) {
+                    resolve(catInfo);
+                } else {
+                    reject('No se encontraron coincidencias para la raza proporcionada');
+
+                    let error = $('<h1 class=" font-bold text-center"></h1>').text('Lo sentimos. No encontramos datos sobre esta raza.');
+                    catPage.append(error);
+                }
+
+                
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                reject(errorThrown);
+            });
     });
-*/
+}
+
+
+function addFavoriteEventListeners(favoriteItem,catBreed) {
+    let currentUser = localStorage.getItem('currentUsername');
+    if(currentUser){
+        let favorites = JSON.parse(localStorage.getItem(`favorites-${currentUser}`)) || [];
+
+        let existingFavorite = favorites.find(cat => cat.breed === catBreed);
+
+        if (!existingFavorite){
+
+            getCatInfo(catBreed)
+            .then(function(catInfo) {
+
+                let cat= {
+                    breed: catInfo.breed,
+                    origin: catInfo.origin,
+                    coat: catInfo.coat,
+                    pattern: catInfo.pattern
+                }
+
+                favorites.push(cat);
+                localStorage.setItem(`favorites-${currentUser}`, JSON.stringify(favorites));
+
+                alert('Added to favorites');
+            })
+            .catch(function(error) {
+                console.error('Error:', error);
+            });
+
+
+        favorites.push(cat);
+        localStorage.setItem(`favorites-${currentUser}`, JSON.stringify(favorites));
+        favoriteItem.text('Added to favorites');
+        favoriteItem.attr('disabled', true);
+        }else{
+            alert('This cat is already in your favorites');
+        }
+
+        
+    }else{
+        alert('You must be logged in to add favorites');
+    }
+    
+}
